82c1c8bc8c466aaad272c769fc8c510b4acc9029
ZHOU <510878689@qq.com>
2022-10-18 19:19:27 +0800

    feat：增加 loadURL 示例代码，隐藏顶部菜单

--- main.js
+++ main.js
@@ -4,9 +4,15 @@ const createWindow = () => {
   const win = new BrowserWindow({
     width: 800,
     height: 600,
+    // frame: false // 隐藏右上角最大化、最小化、关闭按钮
   })
 
   win.loadFile('index.html')
+  // win.loadFile('./dist/index.html')
+  // win.loadURL('http://127.0.0.1:5173/')
+
+  // 隐藏掉顶部菜单
+  win.setMenu(null)
 }
 
 app.whenReady().then(() => {
